424d89a68de1c93739aabfe0a3330d3f64a87de1
MARYAM <127736868+marylishop@users.noreply.github.com>
2024-01-20 12:43:49 +0400

    Delete short_click directory

--- short_click/products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from products import views
-urlpatterns = [
-    path('latest-products/', views.LatestProductsList.as_view()),
-    path('search/', views.search_api),
-    path('<slug:category_slug>/<slug:products_slug>/', views.ProductDetail.as_view()),
-    path('<slug:category_slug>/', views.CategoryDetailView.as_view()),
-]
